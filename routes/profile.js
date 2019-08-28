ee897758d10c22b75fef21104832bffac2a9ea52
Walker Floyd <walkerkeatley@yahoo.com>
2019-08-28 13:13:25 -0700

    no errors on profile route

--- routes/profile.js
+++ routes/profile.js
@@ -2,12 +2,12 @@ const express = require('express');
 const request = require('request');
 const config = require('config');
 const router = express.Router();
-const auth = require('../../middleware/auth');
+const auth = require('../middleware/auth');
 const { check, validationResult } = require("express-validator");
 
-const Profile = require('../../models/Profile');
-const User = require('../../models/User');
-const Post = require('../../models/Post');
+const Profile = require('../models/Profile');
+const User = require('../models/User');
+const Post = require('../models/Post');
 
 // GET api/profile/me
 // @desc Get current users profile
