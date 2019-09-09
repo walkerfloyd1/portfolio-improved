104d3602759ad898dfbc2c73d3f27c4e0b0e1f8f
Walker Floyd <walkerkeatley@yahoo.com>
2019-09-09 13:27:08 -0700

    cleaning up

--- client/src/Pages/photography.js
+++ client/src/Pages/photography.js
@@ -1,17 +1,27 @@
 import React, { Component } from 'react';
 
-import Photos from '../Components/Photos';
-
 import Footer from '../Components/Footer';
 
 import Navbar from '../Components/Navbar';
+
+import ControlledCarousel from '../Components/photoCarousel';
+
+import Grid from '@material-ui/core/Grid';
+
 
 export default class Photo extends Component {
     render() {
       return (
         <div>
           <Navbar />
-              <Photos />
+          <Grid container
+            spacing={0}
+            direction="column"
+            alignItems="center"
+            justify="center"
+            >
+                <Grid item lg={9}><ControlledCarousel /></Grid>
+                </Grid>
             <Footer />
         </div>
       );
