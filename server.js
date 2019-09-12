1f7e712aa3a3c767063585ca296b59636b15f1f4
Walker Floyd <walkerkeatley@yahoo.com>
2019-09-12 11:54:27 -0700

    working on aws deployment

--- server.js
+++ server.js
@@ -23,6 +23,6 @@ if(process.env.NODE_ENV === 'production') {
     })
 }
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8081;
 
 app.listen(PORT, () => console.log("Server started"));
